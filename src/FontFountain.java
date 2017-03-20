51e035aed21e3676cdc494cca5ad0a26dbe185b6
abdullah <abdullah.akalin@gazi.edu.tr>
2017-03-20 10:09:42 +0300

    Visual improvements and fixes on MenuPanel.

--- src/FontFountain.java
+++ src/FontFountain.java
@@ -7,7 +7,7 @@ import java.awt.*;
 public class FontFountain extends Fenestra {
     public FontFountain(Color bgColor, Color captionColor, Color titleColor, String title, int width, int height) {
         super(bgColor, captionColor, titleColor, title, width, height);
-        add(new FontSelector(bgColor, width/3, height), BorderLayout.LINE_START);
+        //add(new FontSelector(bgColor, width/3, height), BorderLayout.LINE_START);
 
         JPanel jpnlLoom = new JPanel(new BorderLayout());
         jpnlLoom.setBackground(bgColor);
--- src/MenuPanel.java
+++ src/MenuPanel.java
@@ -5,6 +5,10 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class MenuPanel extends JPanel {
+    private final Color MENU_FORECOLOR = Palette.middleRedPurple;
+    private final Color MENU_BACKCOLOR = Palette.paynesGrey;
+    private final Font MENU_FONT = new Font("Courier", Font.BOLD, 10);
+
     public MenuPanel() {
         setBackground(Palette.halayaUbe);
         setLayout(new BorderLayout());
@@ -16,24 +20,38 @@ public class MenuPanel extends JPanel {
 
         // JMenuItem arrays for every JMenu:
         ArrayList<JMenuItem> fileMenuItems = new ArrayList<>();
-        JMenuItem jmLoadFont = generateMenuItem("Load font");
-        JMenuItem jmSettings = generateMenuItem("Settings");
+        JMenuItem jmLoadFont = generateMenuItem("LOAD FONT");
+        JMenuItem jmDump = generateMenuItem("DUMP FONT LIST");
+        JMenuItem jmSettings = generateMenuItem("SETTINGS");
         fileMenuItems.add(jmLoadFont);
+        fileMenuItems.add(jmDump);
         fileMenuItems.add(jmSettings);
 
         ArrayList<JMenuItem> viewMenuItems = new ArrayList<>();
-        JMenuItem jmSetBg = generateMenuItem("Background color");
-        JMenuItem jmSetFg = generateMenuItem("Foreground color");
-        viewMenuItems.add(jmSetBg);
-        viewMenuItems.add(jmSetFg);
+        JMenuItem jmSelector = generateMenuItem("FONT SELECTOR");
+        JMenuItem jmComparator = generateMenuItem("FONT COMPARATOR");
+        JMenuItem jmComposer = generateMenuItem("COMPOSER");
+        viewMenuItems.add(jmSelector);
+        viewMenuItems.add(jmComparator);
+        viewMenuItems.add(jmComposer);
+
+        ArrayList<JMenuItem> editMenuItems = new ArrayList<>();
+        JMenuItem jmSetBg = generateMenuItem("BACKGROUND COLOR");
+        JMenuItem jmSetFg = generateMenuItem("FOREGROUND COLOR");
+        JMenuItem jmSetSize = generateMenuItem("DISPLAY SIZE");
+        editMenuItems.add(jmSetSize);
+        editMenuItems.add(jmSetBg);
+        editMenuItems.add(jmSetFg);
 
         // JMenu instances:
-        JMenu fileMenu = generateMenu("File", fileMenuItems);
-        JMenu viewMenu = generateMenu("View", viewMenuItems);
+        JMenu fileMenu = generateMenu("FILE", fileMenuItems);
+        JMenu viewMenu = generateMenu("VIEW", viewMenuItems);
+        JMenu editMenu = generateMenu("EDIT", editMenuItems);
 
         // Adding JMenus to JMenuBar:
         jmbMenu.add(fileMenu);
         jmbMenu.add(viewMenu);
+        jmbMenu.add(editMenu);
 
         // Adding JMenuBar to the panel:
         add(jmbMenu, BorderLayout.LINE_START);
@@ -43,8 +61,8 @@ public class MenuPanel extends JPanel {
         JMenu menu = new JMenu(title);
         menu.setBorderPainted(false);
         menu.getPopupMenu().setBorder(null);
-        menu.setFont(new Font("Courier", Font.BOLD, 10));
-        menu.setForeground(Palette.middleRedPurple);
+        menu.setFont(MENU_FONT);
+        menu.setForeground(MENU_FORECOLOR);
         for(int i = 0; i < menuItems.size(); i++)
             menu.add(menuItems.get(i));
         return menu;
@@ -53,9 +71,9 @@ public class MenuPanel extends JPanel {
     private JMenuItem generateMenuItem(String title) {
         JMenuItem menuItem = new JMenuItem(title);
         menuItem.setBorderPainted(false); // or create a line border.
-        menuItem.setBackground(Palette.paynesGrey);
-        menuItem.setFont(new Font("Courier", Font.BOLD, 10));
-        menuItem.setForeground(Palette.middleRedPurple);
+        menuItem.setBackground(MENU_BACKCOLOR);
+        menuItem.setFont(MENU_FONT);
+        menuItem.setForeground(MENU_FORECOLOR);
         return menuItem;
     }
 }
