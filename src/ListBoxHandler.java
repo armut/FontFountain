d7407bcc610e9366be0d7a5f5efdccccf8accea2
abdullah <abdullah.akalin@gazi.edu.tr>
2017-03-29 08:54:34 +0300

    Added ListBoxHandler class.
    * Related portions of the previous code is moved under this class.

---
+++ src/ListBoxHandler.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+/**
+ * zamma on 29.03.2017.
+ */
+public class ListBoxHandler implements ListSelectionListener, MouseListener {
+    private JList<String> listBox;
+    private ArrayList<Font> fontsList;
+
+    public ListBoxHandler(JList<String> listBox, ArrayList<Font> fontsList) {
+        this.listBox = listBox;
+        this.fontsList = fontsList;
+    }
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+        if(mouseEvent.isPopupTrigger()) {
+            JPopupMenu contextMenu = new JPopupMenu();
+            JMenuItem miApply = new JMenuItem("Apply");
+            JMenuItem miJmp = new JMenuItem("Jump to dir.");
+            miApply.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    System.out.println("Apply");
+                }
+            });
+            miJmp.addActionListener((new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent actionEvent) {
+                    System.out.println("Jmp");
+                }
+            }));
+            contextMenu.add(miApply);
+            contextMenu.add(miJmp);
+            contextMenu.show(mouseEvent.getComponent(), mouseEvent.getX(), mouseEvent.getY());
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void valueChanged(ListSelectionEvent listSelectionEvent) {
+        FontFountain.currentFont = fontsList.get(listBox.getSelectedIndex());
+        //TODO: new Font(...) part of the below have to be beautified.
+        PreviewPanel.textPane.setFont(new Font(FontFountain.currentFont.getName(), Font.PLAIN, 72));
+        //TODO: This method runs two times when the value is changed. Why?
+    }
+}
